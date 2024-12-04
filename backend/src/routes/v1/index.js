9f637499aaf25401c750beeef8708c0f8e47bc5c
Mohak-Trivedi <trivedimohak@gmail.com>
2024-12-04 21:58:19 +0530

    basic controller setup

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { pingCheck } from "../../controllers/pingController";
 
 const router = express.Router();
+
+router.use("/ping", pingCheck);
 
 export default router;
