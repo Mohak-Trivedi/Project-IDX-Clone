c4839b8486fc58966f50ad2b4aa85fbba608442b
Mohak-Trivedi <trivedimohak@gmail.com>
2024-12-04 22:57:57 +0530

    axios setup in frontend

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { pingCheck } from "../../controllers/pingController";
+import { pingCheck } from "../../controllers/pingController.js";
 
 const router = express.Router();
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+import { pingApi } from "../apis/ping";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    pingApi();
+  }, []);
   return <>Hello</>;
 }
 
