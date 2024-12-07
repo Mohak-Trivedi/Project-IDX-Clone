09b9ebd2d93e8403032d9cee0a63f43539e9dfe4
Mohak-Trivedi <trivedimohak@gmail.com>
2024-12-07 15:07:11 +0530

    able to create vite react project on backend

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,14 +1,26 @@
 import { promisify } from "node:util";
 import child_process from "node:child_process";
+import fs from "fs/promises";
+import uuid4 from "uuid4";
 
 const execPromisified = promisify(child_process.exec);
 
 export const createProjectController = async (req, res) => {
-  const { stdout, stderr } = await execPromisified("ls");
-  console.log("stdout:", stdout);
-  console.error("stderr:", stderr);
+  // Create a unique id for each project and create a new folder with this id as
+  // the folder name.
+  const projectId = uuid4();
+  console.log("New project id is: ", projectId);
+
+  await fs.mkdir(`./projects/${projectId}`);
+
+  // In the created project folder, create a vite react project
+  const response = await execPromisified(
+    "npm create vite@latest sandbox -- --template react",
+    { cwd: `./projects/${projectId}` }
+  );
 
   return res.json({
     message: "Project created",
+    data: projectId,
   });
 };
