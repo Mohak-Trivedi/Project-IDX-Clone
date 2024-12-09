a3f4f2df6bb1fa78c1a5946a95a90560d35f46bf
Mohak-Trivedi <trivedimohak@gmail.com>
2024-12-09 22:11:48 +0530

    moved npm vite react cmd from controller to env

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -2,6 +2,7 @@ import { promisify } from "node:util";
 import child_process from "node:child_process";
 import fs from "fs/promises";
 import uuid4 from "uuid4";
+import { REACT_PROJECT_COMMAND } from "../../config/serverConfig.js";
 
 const execPromisified = promisify(child_process.exec);
 
@@ -14,10 +15,9 @@ export const createProjectController = async (req, res) => {
   await fs.mkdir(`./projects/${projectId}`);
 
   // In the created project folder, create a vite react project
-  const response = await execPromisified(
-    "npm create vite@latest sandbox -- --template react",
-    { cwd: `./projects/${projectId}` }
-  );
+  const response = await execPromisified(REACT_PROJECT_COMMAND, {
+    cwd: `./projects/${projectId}`,
+  });
 
   return res.json({
     message: "Project created",
