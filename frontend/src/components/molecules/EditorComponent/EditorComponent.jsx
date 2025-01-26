90413d89ee75a027fa934ef075178c4a833325d0
Mohak-Trivedi <trivedimohak@gmail.com>
2025-01-26 16:12:46 +0530

    added theme to editor

--- frontend/src/components/molecules/EditorComponent/EditorComponent.jsx
+++ frontend/src/components/molecules/EditorComponent/EditorComponent.jsx
@@ -1,8 +1,32 @@
 import Editor from "@monaco-editor/react";
+import { useState, useEffect } from "react";
+import themeData from "../../../../Dracula.json";
 
 export const EditorComponent = () => {
+  // const [editorState, setEditorState] = useState({
+  //   theme: null,
+  // });
+
+  // async function downloadTheme() {
+  //   const response = await fetch("../../../../Dracula.json");
+  //   const data = await response.json();
+  //   console.log(data);
+  //   setEditorState({ ...editorState, theme: data });
+  // }
+
+  function handleEditorTheme(editor, monaco) {
+    // monaco.editor.defineTheme("dracula", editorState.theme);
+    monaco.editor.defineTheme("dracula", themeData);
+    monaco.editor.setTheme("dracula");
+  }
+
+  // useEffect(() => {
+  //   downloadTheme();
+  // }, []);
+
   return (
     <>
+      {/* {editorState.theme && ( */}
       <Editor
         height="80vh"
         width="100%"
@@ -12,7 +36,9 @@ export const EditorComponent = () => {
           fontSize: 18,
           fontFamily: "monospace",
         }}
+        onMount={handleEditorTheme}
       />
+      {/* )} */}
     </>
   );
 };
